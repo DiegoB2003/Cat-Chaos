e0c2db01a8dc6823d3e0283f32876c40cf067a0e
Nick Filipov <108493392+Nickick10@users.noreply.github.com>
2025-03-15 16:44:52 -0500

    Added player movement to cat and collision

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    public float speed = 5.0f;
+    public float rotationSpeed = 90;
+    public float force = 250f;
+
+    Rigidbody rb;
+    Transform t;
+    bool isGrounded = false; //Check if player is grounded
+
+    public Vector2 turn; //Vector2 stores direction of the mouse movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked; //Locks mouse in place and makes it invisible
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime represents the time that passed since the last frame
+        //the multiplication below ensures that GameObject moves constant speed every frame
+        Vector3 moveDirection = new Vector3(0, 0, 0);
+
+        moveDirection += Input.GetKey(KeyCode.W) ? transform.forward : Vector3.zero;
+        moveDirection -= Input.GetKey(KeyCode.S) ? transform.forward : Vector3.zero;
+        moveDirection += Input.GetKey(KeyCode.D) ? transform.right : Vector3.zero;
+        moveDirection -= Input.GetKey(KeyCode.A) ? transform.right : Vector3.zero;
+
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + moveDirection * speed * Time.deltaTime);
+
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        turn.y = Mathf.Clamp(turn.y, -90, 90); // Clamp the vertical rotation
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+        Camera.main.transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
+
+        //Jump only if grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * force);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) //If player is grounded, set isGrounded to true
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision) //If player is not grounded, set isGrounded to false
+    {
+        isGrounded = false;
+    }
+}

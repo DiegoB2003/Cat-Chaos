b72c5da80d20e2c56e044478798b8f70534ecbd7
Nick Filipov <108493392+Nickick10@users.noreply.github.com>
2025-04-05 14:51:28 -0500

    Homeowner and AI
    Created homeowner model, animated Idle, walking, and running, and made him idle, patrol randomly, and chase player when they get too close, and go back to patrolling when too far

---
+++ Assets/Scripts/HomeownerAIScript.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class HomeownerAIScript : MonoBehaviour
+{   
+    private Animator anim;
+    private bool isWaiting = false;
+
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("PlayerCat").transform;
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrol();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patrol()
+    {   
+        if (!walkPointSet && !isWaiting) SearchWalkPoint();
+
+        if (walkPointSet && !isWaiting)
+        {
+            agent.SetDestination(walkPoint);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isWalking", true);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (!isWaiting && walkPointSet && distanceToWalkPoint.magnitude < 1f)
+        {
+            StartCoroutine(IdleBeforeNextWalk());
+        }
+    }
+
+    private IEnumerator IdleBeforeNextWalk()
+    {
+        isWaiting = true;
+        walkPointSet = false;
+
+        agent.SetDestination(transform.position); //Stop moving
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isWalking", false); //Play idle animation
+
+        yield return new WaitForSeconds(5f); //Wait for 5 seconds
+
+        isWaiting = false;
+    }
+
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+            agent.SetDestination(walkPoint);
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        anim.SetBool("isRunning", true);
+        anim.SetBool("isWalking", false);
+    }
+
+    private void AttackPlayer()
+    {   
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isWalking", false);
+
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Add attack code here depending on what we want later
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+}

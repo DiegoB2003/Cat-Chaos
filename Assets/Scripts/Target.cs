43cfda0cb7d8ab460e9380e932eb90a9c4792135
dbravo4 <dbravo4@uic.edu>
2025-04-14 18:53:08 -0500

    fixed owner attack audio The audio source didn't exist, now it does.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,15 +6,33 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public Score scoreManager;
+    public float destructionForceThreshold = 100f;
  
     //this method is called whenever a collision is detected
     private void OnCollisionEnter(Collision collision) {
         //on collision adding point to the score
+        if(collision.gameObject.layer == 6){
+            // printing if collision is detected on the console
+            Debug.Log("Collision Detected");
+
+            // Get the total impulse from the collision
+            Vector3 impulse = collision.impulse;
+            float impactForce = impulse.magnitude / Time.fixedDeltaTime;
+
+            Debug.Log($"Impact Force: {impactForce}");
+
+            // Check if the force is over the threshold
+            if (impactForce >= destructionForceThreshold)
+            {
+                DestroyObject();
+            }
+        }
+    }
+
+    void DestroyObject()
+    {
+        Debug.Log($"{gameObject.name} was destroyed due to high impact! Point added.");
         scoreManager.AddPoint();
- 
-        // printing if collision is detected on the console
-        Debug.Log("Collision Detected");
-        //after collision is detected destroy the gameobject
-        Destroy(gameObject);
+        Destroy(gameObject); // Could also spawn particles, sounds, etc.
     }
 }

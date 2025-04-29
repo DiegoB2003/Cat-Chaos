4f20b85ee80b8ac7b621b991c6d77002357a21c5
Arlette Diaz <adiaz218@uic.edu>
2025-04-29 12:36:43 -0500

    Noise meter update
    Made it more clear that the meter is a noise meter and when the player is in danger

--- Assets/Scripts/NoiseMeterUI.cs
+++ Assets/Scripts/NoiseMeterUI.cs
@@ -6,7 +6,12 @@ public class NoiseMeterUI : MonoBehaviour
     [SerializeField] private Slider noiseSlider;
     [SerializeField] private Image fillImage;
 
-    void Update()
+    [Header("Flashing Settings")]
+    [SerializeField] private float dangerThreshold = 0.7f;
+    [SerializeField] private float flashSpeed = 2f;
+    private bool isFlashing = false;
+
+    private void Update()
     {
         float currentNoiseLevel = NoiseManager.Instance.GetNoiseLevel();
         float maxNoise = NoiseManager.Instance.GetMaxNoise();
@@ -14,6 +19,22 @@ public class NoiseMeterUI : MonoBehaviour
         float noisePercentage = currentNoiseLevel / maxNoise;
         noiseSlider.value = noisePercentage * 100f;
 
-        fillImage.color = Color.Lerp(Color.green, Color.red, noisePercentage);
+        // Start/Stop flashing based on threshold
+        if (noisePercentage >= dangerThreshold)
+        {
+            isFlashing = true;
+        }
+        else
+        {
+            isFlashing = false;
+            fillImage.color = Color.Lerp(Color.green, Color.red, noisePercentage); // reset to static color
+        }
+
+        // Flashing effect
+        if (isFlashing)
+        {
+            float t = Mathf.PingPong(Time.time * flashSpeed, 1f);
+            fillImage.color = Color.Lerp(Color.red, Color.yellow, t);
+        }
     }
 }
